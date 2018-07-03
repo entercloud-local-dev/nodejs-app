81ae1f7230b9713bd6e9b4cd682ed6e2b1fd1394
ehime <dodomeki@gmail.com>
2018-07-02 20:19:23 -0700

    saner ami, updating rake, preppingv test for conversion

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -xe
-# serverspec.sh - RSpec tests for servers
-echo -e "==> Installing Serverspec"
-echo sudo gem install serverspec
 
 # move and set rake-handler
 # bypasses /opt/sensu/embedded/bin/rake vs /usr/bin/rake etc
 cd /tmp/tests ; rake=$(which rake |head -n1 |awk '{print$3}')
 echo -e "==> Running integration tests for AMI"
-echo $rake spec
+
+$rake spec
