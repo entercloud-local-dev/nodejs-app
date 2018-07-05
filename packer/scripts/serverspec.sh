d3207ac133dccbe846b9f5f8d48fb9eb6a7414ff
ehime <dodomeki@gmail.com>
2018-07-05 10:53:07 -0700

    moving back to ntp

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -xe
 
+export TARGET_HOST=127.0.0.1
+
 # move and set rake-handler
 # bypasses /opt/sensu/embedded/bin/rake vs /usr/bin/rake etc
 cd /tmp/tests ; rake=$(which rake |head -n1 |awk '{print$1}')
 echo -e "==> Running integration tests for AMI"
 
-$rake spec
+$rake default:serverspec
