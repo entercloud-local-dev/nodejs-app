4e479400e490222a7f3fe5a0732647fd249a659a
ehime <dodomeki@gmail.com>
2018-07-05 12:30:17 -0700

    basic tests

--- packer/scripts/serverspec.sh
+++ packer/scripts/serverspec.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -xe
 
-export TARGET_HOST=127.0.0.1
-
 # move and set rake-handler
 # bypasses /opt/sensu/embedded/bin/rake vs /usr/bin/rake etc
 cd /tmp/tests ; rake=$(which rake |head -n1 |awk '{print$1}')
 echo -e "==> Running integration tests for AMI"
 
-$rake default:serverspec
+$rake serverspec:localhost
